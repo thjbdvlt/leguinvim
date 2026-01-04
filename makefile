0fa18f726ac1a844a43ceae1be802b174c62a16a
thjbbvlt <thibault2@ik.me>
2026-01-04 17:48:55 +0100

    reduce font size of lines too large for window
    lines that are too longs with non-monospace fonts, for example:
    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
    are now rendered in smaller font. the font size is dynamically
    computed so that the text is not too small (if possible).
    
    + minor changes
    + update readme
    + bump to 0.0.6

--- makefile
+++ makefile
@@ -5,7 +5,8 @@ src := $(shell find -name '*.rs')
 install_path := $(DEST_DIR)$(PREFIX)/bin/$(name)
 
 all: $(bin)
-	./$(bin) u-k*
+	# ./$(bin) u-k*
+	./$(bin) toolong.md
 
 $(bin): $(src) Cargo.toml
 	cargo build --release
--- src/pix_grid.rs
+++ src/pix_grid.rs
@@ -5,6 +5,18 @@ pub const GRID_WIDTH_RATIO: f64 = 0.9;
 pub type PixLine = Box<[f32]>;
 type PixMatrix = Box<[PixLine]>;
 
+macro_rules! unscale {
+    ($e: expr) => {
+        ($e / pango::SCALE) as f32
+    };
+}
+
+macro_rules! width {
+    ( $e: expr ) => {
+        unscale!($e.width())
+    };
+}
+
 #[derive(Default, Debug)]
 pub struct PixModel {
     pub matrix: PixMatrix,
@@ -12,79 +24,70 @@ pub struct PixModel {
     rows: usize,
 }
 
-fn new_pix_matrix(columns: usize, rows: usize, space_width: f32) -> PixMatrix {
-    vec![vec![space_width; columns + 1].into_boxed_slice(); rows].into_boxed_slice()
-}
-
 impl PixModel {
     pub fn new(columns: usize, rows: usize) -> Self {
         PixModel {
-            matrix: new_pix_matrix(columns, rows, 0.0),
             columns,
             rows,
-            ..PixModel::default()
+            matrix: vec![vec![0.0; columns + 1].into_boxed_slice(); rows].into_boxed_slice(),
         }
     }
 
     pub fn from_grid(model: &UiModel, space_width: f32, sign_column: usize) -> Self {
         let model = model;
         let (rows, columns) = (model.rows, model.columns);
-        let matrix = new_pix_matrix(columns, rows, space_width);
-        let mut pix_model = PixModel {
-            matrix,
-            columns,
-            rows,
-            ..PixModel::default()
-        };
+        let mut pix_model = PixModel::new(columns, rows);
         for (row, line) in model.model().iter().enumerate() {
-            pix_model.update_line(line, row, space_width, sign_column);
+            pix_model.update(line, row, space_width, sign_column);
         }
         pix_model
     }
 
-    pub fn update_line(&mut self, line: &Line, row: usize, space_width: f32, sign_column: usize) {
+    pub fn update(&mut self, line: &Line, row: usize, space_width: f32, sign_column: usize) -> f32 {
         let pix_line = &mut self.matrix[row];
         pix_line.fill(space_width);
+        let mut last_non_space: usize = sign_column;
+        // TODO optimize with filter/map/filter_map/flat_map?
         for col in sign_column..self.columns {
             for item in &line.item_line[col] {
                 let glyphs = item.glyphs();
                 if glyphs.is_some() {
                     for glyph_string in glyphs.iter() {
                         let n = glyph_string.num_glyphs();
-                        pix_line[col] = (glyph_string.width() / pango::SCALE) as f32;
+                        pix_line[col] = width!(glyph_string);
+                        last_non_space = col + n as usize;
                         if n > 1 {
                             /* when multiple glyphs are computed as one, we set the
                              * subsequent glyphs length to zero.
-                             * */
-                            for i in 1..n as usize {
-                                pix_line[col + i] = 0.0;
-                            }
+                             */
+                            pix_line[col + 1..last_non_space].fill(0.0);
                         }
                     }
                 }
             }
         }
-        self.len_to_pos(row);
+        self.len_to_pos(row, last_non_space + 1)
     }
 
-    pub fn update_line_monospace(&mut self, row: usize, space_width: f32) {
+    pub fn update_mono(&mut self, row: usize, space_width: f32) -> f32 {
         self.matrix[row].fill(space_width);
-        self.len_to_pos(row);
+        self.len_to_pos(row, self.matrix[row].len())
     }
 
-    fn len_to_pos(&mut self, row: usize) {
+    fn len_to_pos(&mut self, row: usize, last_non_space: usize) -> f32 {
         /* compute the horizontal position in pixel of each cell
          * we just reuse the same array here, because the previous one
-         * is not usefull anymore
-         * */
+         * is not usefull anymore.
+         */
         let pix_line = &mut self.matrix[row];
         let mut x: f32 = 0.0;
-        for i in 0..self.columns {
+        for i in 0..last_non_space {
             let len = pix_line[i];
             pix_line[i] = x;
             x += len;
         }
         pix_line[self.columns] = pix_line[self.columns - 1];
+        x
     }
 
     pub fn fit(&self, model: &UiModel) -> bool {
@@ -120,7 +123,7 @@ pub fn cursor_x(
     let mut col: usize = sign_column_len;
     let mut cursor_width: f32 = space_width;
     let mut word_width_until: f32 = 0.0;
-    // TODO macro ... ($x.width() / pango::scale) as f32
+    // TODO optimize
     'l: loop {
         for item in &line.item_line[col] {
             let glyphs = item.glyphs();
@@ -132,11 +135,12 @@ pub fn cursor_x(
                         if m > 0 {
                             let mut new_glyph = glyph_string.clone();
                             new_glyph.set_size(m as i32);
-                            x += (new_glyph.width() / pango::SCALE) as f32;
-                            word_width_until = (new_glyph.width() / pango::SCALE) as f32;
+                            let w = width!(new_glyph);
+                            x += w;
+                            word_width_until = w;
                             n_glyphs += m;
                             if let Some(c) = glyph_string.glyph_info().iter().nth(m) {
-                                cursor_width = (c.geometry().width() / pango::SCALE) as f32;
+                                cursor_width = width!(c.geometry());
                             }
                         }
                         break 'l;
@@ -146,7 +150,7 @@ pub fn cursor_x(
                          * but ensure that it's computed the same way as it is for
                          * line drawing
                          * */
-                        x += (glyph_string.width() / pango::SCALE) as f32;
+                        x += width!(glyph_string);
                     }
                 }
             }
@@ -155,7 +159,7 @@ pub fn cursor_x(
         if col >= cursor_col {
             if col < line.item_line.len() {
                 if let Some(width) = width_word_first_char(line, col) {
-                    cursor_width = (width / pango::SCALE) as f32;
+                    cursor_width = unscale!(width);
                 }
             }
             break 'l;
